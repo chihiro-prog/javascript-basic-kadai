4ca6c23a4e562610c6af27660c0254b9be30244d
chihiro-prog <147376854+chihiro-prog@users.noreply.github.com>
2023-11-30 20:43:47 +0900

    kadai_mdn_jsをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+// 
+// const  today=Date();
+// const dateString = (year, monthIndex, day) => {
+    // console.log(dateString);
+// }
+
+const today = new Date(2023,8,22);
+console.log(today.getFullYear() +"年");
+console.log(today.getMonth() + "月");
+console.log(today.getDate()+"日");

237f7593983af0f20a9aeb41c1093d05d975f246
chihiro-prog <147376854+chihiro-prog@users.noreply.github.com>
2023-12-04 16:56:47 +0900

    kadai_020を再提出

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,6 +1,11 @@
 const changeText = document.getElementById("btn");
+// const parent = document.querySelector("body");
 
 changeText.addEventListener("click", () => {
-    console.log("ボタンをクリックしました");
-    
+    // console.log("ボタンをクリックしました");
+    //   text.style.display = "none";
+    // const text2 = document.createElement("h2");
+    const text = document.getElementById("text");
+    text.textContent = "ボタンをクリックしました";
+    // parent.appendChild(text2);
 });
